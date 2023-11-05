12e505e53a2f1585978a326d9d43a0fbc64048b5
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-05 21:23:32 +0200

    Space hold down -> Hit the enemy for each attack animation played .

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,18 +7,40 @@ public class PlayerAttack : MonoBehaviour
     public Animator animator;
 
     public Transform attackPoint;
-    public float attackRange = 1f;
     public LayerMask enemyLayers;
 
     public int damage = 20;
+    public float attackRange = 1f;
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+    private bool alreadyAttacking = false;
+    private float secondInAttackRateFormula = 1f;
 
+    void Start()
+    {
+        damage = 20;
+        attackRange = 1f;
+        attackRate = 2f;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Time.time >= nextAttackTime)
         {
-            Attack();
-        } 
+            // verifici daca deja ataca ( deja este butonul de atac apasat ), atunci faci direct damage.
+            if (alreadyAttacking)
+            {
+                Attack();
+                nextAttackTime = Time.time + secondInAttackRateFormula / attackRate;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("sunt intrat pe space down");
+                Attack();
+                nextAttackTime = Time.time + secondInAttackRateFormula / attackRate;
+            }
+        }
+        
         if (Input.GetKeyUp(KeyCode.Space))
         {
             StopAttacking();
@@ -29,6 +51,7 @@ public class PlayerAttack : MonoBehaviour
     {
         // Attack animation
         animator.SetTrigger("Attacking");
+        alreadyAttacking = true;
 
         // Detect enemies in range of attack
         Collider2D[] hitedEnemies =  Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
@@ -39,8 +62,10 @@ public class PlayerAttack : MonoBehaviour
             enemy.GetComponent<Enemy>().TakeDamage(damage);
         }
     }
+
     private void StopAttacking()
     {
         animator.ResetTrigger("Attacking");
+        alreadyAttacking = false;
     }
 }
