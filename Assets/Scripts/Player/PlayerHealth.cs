978c046b577ba380a52dcf1dd2d5a46dd0c96c74
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-08 16:40:25 +0200

    stop cronos when player dies 2

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,16 +5,21 @@ public class PlayerHealth : MonoBehaviour
     public Animator Animator;
     public Healthbar Healthbar;
     public GameManager GameManager;
+    
+    private Cronos cronos;
 
     public int maxHealth = 100;
     public int currentHealth;
+
 
     void Start()
     {
         currentHealth = maxHealth;
         Healthbar.SetMaxHealth(maxHealth);
-    }
 
+        var cronosText = GameObject.Find("CronosText");
+        cronos = cronosText.GetComponent<Cronos>();
+    }
     public void TakeDamage(int damage)
     {
         if (currentHealth > 0)
@@ -34,7 +39,10 @@ public class PlayerHealth : MonoBehaviour
 
     private void Die()
     {
+
         Animator.SetBool("IsDead", true);
+
+        cronos.StopCounter();
 
         GetComponent<Collider2D>().enabled = false;
         GetComponent<BoxCollider2D>().enabled = false;
